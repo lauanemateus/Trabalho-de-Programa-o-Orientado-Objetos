38367ef538801671c56dd4b7f2a0dc0e4bcbf986
lauanemateus <88512406+lauanemateus@users.noreply.github.com>
2024-01-28 22:03:25 -0300

    Update Tamanho.java

--- lojavirtual/Tamanho.java
+++ lojavirtual/Tamanho.java
@@ -1,32 +1,58 @@
 package lojavirtual;
 
-public class Tamanho {
-	private int quantidadeUnidades = 0;
-
-	// construtor vazio
-	public Tamanho() {
+public class Estoque {
+	Tamanho tamanhoPP = new Tamanho();
+	Tamanho tamanhoP = new Tamanho();
+	Tamanho tamanhoM = new Tamanho();
+	Tamanho tamanhoG = new Tamanho();
+	Tamanho tamanhoGG = new Tamanho();
+	Tamanho tamanhoXG = new Tamanho();
+	
+	// set uma quantidade geral para todos tamanhos
+	public void setTodosTamanhos(int quantidadeUnidades) {
+		tamanhoPP.setQuantidadeUnidades(quantidadeUnidades);
+		tamanhoP.setQuantidadeUnidades(quantidadeUnidades);
+		tamanhoM.setQuantidadeUnidades(quantidadeUnidades);
+		tamanhoG.setQuantidadeUnidades(quantidadeUnidades);
+		tamanhoGG.setQuantidadeUnidades(quantidadeUnidades);
+		tamanhoXG.setQuantidadeUnidades(quantidadeUnidades);
 	}
 	
-	// construtor
-	public Tamanho(int quantidadeUnidades) {
-		this.quantidadeUnidades = quantidadeUnidades;
+	// getters e setters
+	public Tamanho getTamanhoPP() {
+		return tamanhoPP;
 	}
-	
-	// get e set
-	public int getQuantidadeUnidades() {
-		return quantidadeUnidades;
+	public void setTamanhoPP(Tamanho tamanhoPP) {
+		this.tamanhoPP = tamanhoPP;
 	}
-	public void setQuantidadeUnidades(int quantidadeUnidades) {
-		this.quantidadeUnidades = quantidadeUnidades;
+	public Tamanho getTamanhoP() {
+		return tamanhoP;
 	}
-	
-	// incrementar quantidade no estoque desse tamanho
-	public void somarQuantidade(int quantidadeParaSomar) {
-		this.quantidadeUnidades += quantidadeParaSomar;
+	public void setTamanhoP(Tamanho tamanhoP) {
+		this.tamanhoP = tamanhoP;
 	}
-	
-	// decrementar quantidade no estoque desse tamanho
-	public void subtrairQuantidade(int quantidadeParaSubtrair) {
-		this.quantidadeUnidades -= quantidadeParaSubtrair;
+	public Tamanho getTamanhoM() {
+		return tamanhoM;
+	}
+	public void setTamanhoM(Tamanho tamanhoM) {
+		this.tamanhoM = tamanhoM;
+	}
+	public Tamanho getTamanhoG() {
+		return tamanhoG;
+	}
+	public void setTamanhoG(Tamanho tamanhoG) {
+		this.tamanhoG = tamanhoG;
+	}
+	public Tamanho getTamanhoGG() {
+		return tamanhoGG;
+	}
+	public void setTamanhoGG(Tamanho tamanhoGG) {
+		this.tamanhoGG = tamanhoGG;
+	}
+	public Tamanho getTamanhoXG() {
+		return tamanhoXG;
+	}
+	public void setTamanhoXG(Tamanho tamanhoXG) {
+		this.tamanhoXG = tamanhoXG;
 	}
 }
